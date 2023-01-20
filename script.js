896719b2fcb13bb7d2f88f2e4a04cb36d69d8d29
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-20 22:35:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ let text = document.getElementById('evaluatedText')
 let count = document.getElementById('wordCount')
 
 text.addEventListener("input", () => {
-	let word = text.value
-	let splitedWord = word.replace(/[^a-zA-Z0-9\s]/g, "").split(/\s+/);
+	let word = text.value.trim()
+	if(word.length === 0){
+		count.innerText = 0
+		return 
+	}
+	let splitedWord = word.replace(/[^a-zA-Z0-9\s]/g, "").split(/\s+/).filter(Boolean);
 	count.innerText = splitedWord.length
 })
