dfb37ebf25e2077c174ca93d9f518aaeeb50fe4b
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-20 22:13:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+let text = document.getElementById('evaluatedText')
+let count = document.getElementById('wordCount')
+
+text.addEventListener("input", () => {
+	count.innerText = text.value.length
+})
